c68674117e58802d9c2274d44f61513a82d37188
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2022-09-25 21:39:07 +0300

    Update BookController.java

--- BookController.java
+++ BookController.java
@@ -15,25 +15,47 @@ import java.util.Map;
 public class BookController {
     Map<Integer, Book > data =new HashMap<>();
 
+     private int id;
+    private  String name;
+
+
+
+
+
+
     @PostConstruct
     void init(){
-        data.put(1, new Book( id : 1, name : "Domovodstvo"));
-        data.put(1, new Book( id : 2, name : "Sadovodstvo"));
+        data.put(1, new Book(id: 1, name: "Domovodstvo"));
+        data.put(2, new Book(id: 2, name: "Sadovodstvo"));
+        data.put(3, new Book(id: 3, name: "Kulinaria"));
     }
+
     @GetMapping("/{id}")
-    public Book (@PathVariable int id) {
+    public Book get(@PathVariable int id) {
         return data.get(id);
     }
 
     @GetMapping("/all")
-    public List<Book> getAllBooks() {
+    public List<Book> get() {
         return new ArrayList<>(data.values());
     }
 
     @PostMapping()
-    public void saveBook(@RequestBody Book book) {
+    public void save(@RequestBody Book book) {
         int id = data.size() + 1;
         book.setID(id);
         data.put(id, book);
+    }
+
+
+    @PutMapping("/{id}")
+    public void change(@PathVariable int id, @RequestBody Book bookChanding){
+            Book  book= data.get(id);
+            book.setName(bookChanding.getName());
+            data.put(id,book);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        data.remove(id);
     }
 }
