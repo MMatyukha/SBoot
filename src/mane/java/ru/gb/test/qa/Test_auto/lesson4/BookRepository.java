a72ad9472db5849ebd896964ed10c9b27338286b
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2022-10-05 21:54:36 +0300

    Update BookRepository.java

--- src/mane/java/ru/gb/test/qa/Test_auto/lesson4/BookRepository.java
+++ src/mane/java/ru/gb/test/qa/Test_auto/lesson4/BookRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
-    Optional<BookEntity>findByNameAndid(String Name, int secondName);
+    Optional<BookEntity>findByNameAndid(String Name);
 }
 
 
