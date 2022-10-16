b5cf4d5fb29b9222dc182a22f4670c8192f78772
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2022-10-16 19:17:55 +0300

    Add files via upload

---
+++ src/test/GeekBrainsTestConfig.java
@@ -0,0 +1,9 @@
+package ru.gb.test.qa.Test_auto;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@TestConfiguration
+@ComponentScan(ru.gb.test.qa.Test_auto.lesson7)
+public class GeekBrainsTestConfig {
+}
---
+++ src/test/TestAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.test.qa.Test_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
